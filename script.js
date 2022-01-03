237ba1d262fae24e5e5519a4a17e29819d94e003
Gary Su <garysu929292@gmail.com>
2022-01-03 16:16:50 -0500

    added if statements to check who won the round

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function computerMove(e) {
   let select = Math.floor(Math.random() * 3) + 1;
-  let comp;
+  let comp, result;
   if (select == 1) {
     comp = "rock";
   } else if (select == 2) {
@@ -8,6 +8,15 @@ function computerMove(e) {
   } else {
     comp = "scissor";
   }
+  let user = e.target.className;
+  if (comp == user) result = "tie";
+  else if (
+    (comp == "rock" && user == "scissor") ||
+    (comp == "scissor" && user == "paper") ||
+    (comp == "paper" && user == "rock")
+  )
+    result = "loss";
+  else result = "win";
 }
 
 window.addEventListener("click", computerMove);

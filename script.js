1e8c79a7846c9af696da74e80f3bf3aa1d3ca95f
Gary Su <garysu929292@gmail.com>
2022-01-03 19:33:40 -0500

    allowed the game to reset after 5 moves

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
+function updateScore(result) {
+  if (result == "win") userScore++;
+  else if (result == "loss") compScore++;
+  else ties++;
+  document.getElementById("header").innerHTML =
+    "Your wins: " +
+    userScore +
+    "\t Computer wins: " +
+    compScore +
+    "\t Ties: " +
+    ties;
+  document.getElementById("result").innerHTML = "";
+  document.getElementById("result").innerHTML = result;
+  played++;
+  if (played == 5) {
+    if (compScore > userScore) alert("You lost");
+    else if (compScore < userScore) alert("You won");
+    else alert("Tie");
+    played = 0;
+    compScore = 0;
+    userScore = 0;
+    ties = 0;
+    document.getElementById("header").innerHTML =
+      "Your wins: " + 0 + "\t Computer wins: " + 0 + "\t Ties: " + 0;
+    document.getElementById("result").innerHTML =
+      "Click one of the above to start!";
+  }
+}
+
 function computerMove(e) {
+  if (e.target.className !== "icon") return;
   let select = Math.floor(Math.random() * 3) + 1;
   let comp, result;
   if (select == 1) {
@@ -8,7 +38,7 @@ function computerMove(e) {
   } else {
     comp = "scissor";
   }
-  let user = e.target.className;
+  let user = e.target.id;
   if (comp == user) result = "tie";
   else if (
     (comp == "rock" && user == "scissor") ||
@@ -17,9 +47,20 @@ function computerMove(e) {
   )
     result = "loss";
   else result = "win";
+  updateScore(result);
 }
-let userScore = 0,
-  compScore = 0;
-console.log(document.getElementById("header"));
 
+let userScore = 0,
+  compScore = 0,
+  ties = 0;
+played = 0;
+document.getElementById("header").innerHTML =
+  "Your wins: " +
+  userScore +
+  "\t Computer wins: " +
+  compScore +
+  "\t Ties: " +
+  ties;
+document.getElementById("result").innerHTML =
+  "Click one of the above to start!";
 window.addEventListener("click", computerMove);
